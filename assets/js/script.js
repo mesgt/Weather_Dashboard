8291135be1e9102992ad87778a4f5b2673be018a
Maria Sargent <maria.marshall.83@gmail.com>
2021-01-12 22:58:53 -0600

    Pseudocode added.

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+//pseudocode:
+//The app will come up with a search field where the user will enter the location and click 'search'
+    //start with 2 columns (3/9). First column is going to have a search box with "Search for a City:" and below it links to 8 largest cities in US. Austin TX, Chicago IL, New York NY, Orlando FL, San Francisco CA, Seattle WA, Denver CO, Atlanta GA.
+    //Searched location is added to the search history, which will add the city searched to the left column (append).
+    //When user clicks on location previously searched, they are presented with current and future conditions for that location. 
+
+//The app then displays the current and future weather conditions: city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index (color indicative)
+    //the 2nd column is going to show the last search that was made by the user. 
+    //5 day forecast will have the following: date, an icon representation of weather conditions, the temperature, and the humidity
+
+
+
+
+
+
+
